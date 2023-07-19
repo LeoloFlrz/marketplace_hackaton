7c089238589ae7e3f8f91ee6d682c769d3cf79c7
Olga G <oldchatot@gmail.com>
2023-07-19 17:38:09 +0200

    Landing page

---
+++ araba_marketplace_front/src/components/ofertaCard.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import car from '../img/CAR-1.svg'
+
+export default function CardOferta () {
+    return (
+        <>
+        <div>
+            <img src={car} alt="imagen" />
+            <h3 className="modelo"></h3>
+            <div>
+                <div></div>
+                <div></div>
+            </div>
+            <div>
+                <div className="precio"></div>
+                <div>
+                    <button>Me interesa</button>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
