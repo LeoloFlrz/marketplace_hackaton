0910805f5e7a77b7bb2f227f7024e4d21d8b9cc6
denizozerdogan <131254999+denizozerdogan@users.noreply.github.com>
2023-07-19 19:53:14 +0200

    changed service for mongoose testing ongoing

--- marketplace_backend/src/cars/cars.service.spec.ts
+++ marketplace_backend/src/cars/cars.service.spec.ts
@@ -1,13 +1,28 @@
-import { Test, TestingModule } from '@nestjs/testing';
+/* import { Test, TestingModule } from '@nestjs/testing';
 import { CarsService } from './cars.service';
+import { Model, Schema } from 'mongoose';
+import { Car } from './schemas/car.schema';
+import { getModelToken } from '@nestjs/mongoose';
 
 describe('CarsService', () => {
   let service: CarsService;
+  let carModel: Model<Car>;
+
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CarsService],
+      providers: [CarsService,
+        {
+          provide: getModelToken(Car.name),
+          useValue: {
+            find: jest.fn().mockReturnThis(),
+            findById: jest.fn().mockReturnThis(),
+            exec: jest.fn(),
+          },
+        },
+      ],
     }).compile();
+
 
     service = module.get<CarsService>(CarsService);
   });
@@ -15,4 +30,23 @@ describe('CarsService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('findAll', () => {
+    it('should return an array of cars', async () => {
+      const expectedCars: Car[] = [
+        // Define your expected cars here
+      ];
+
+      jest.spyOn(carModel, 'find').mockReturnValue({
+        exec: jest.fn().mockResolvedValue(expectedCars),
+      } as any);
+
+      const result = await carsService.findAll();
+
+      expect(result).toEqual(expectedCars);
+      expect(carModel.find).toHaveBeenCalled();
+    });
+  });
+
 });
+ */
