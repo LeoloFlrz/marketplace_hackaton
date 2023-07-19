6e072e40117f69fcdd1c89986e088877634e6c18
Leolo Flórez <116220158+LeoloFlrz@users.noreply.github.com>
2023-07-19 13:32:23 +0200

    fixed json import

--- marketplace_backend/src/cars/cars.controller.ts
+++ marketplace_backend/src/cars/cars.controller.ts
@@ -1,16 +1,16 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { CarsService } from './cars.service';
-import { CreateCarDto } from './dto/create-car.dto';
-import { UpdateCarDto } from './dto/update-car.dto';
+// import { CreateCarDto } from './dto/create-car.dto';
+// import { UpdateCarDto } from './dto/update-car.dto';
 
 @Controller('cars')
 export class CarsController {
   constructor(private readonly carsService: CarsService) {}
 
-  @Post()
-  create(@Body() createCarDto: CreateCarDto) {
-    return this.carsService.create(createCarDto);
-  }
+  // @Post()
+  // create(@Body() createCarDto: CreateCarDto) {
+  //   return this.carsService.create(createCarDto);
+  // }
 
   @Get()
   findAll() {
@@ -22,13 +22,13 @@ export class CarsController {
     return this.carsService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCarDto: UpdateCarDto) {
-    return this.carsService.update(+id, updateCarDto);
-  }
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateCarDto: UpdateCarDto) {
+  //   return this.carsService.update(+id, updateCarDto);
+  // }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.carsService.remove(+id);
-  }
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.carsService.remove(+id);
+  // }
 }
--- marketplace_backend/src/cars/cars.service.ts
+++ marketplace_backend/src/cars/cars.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@nestjs/common';
-import { CreateCarDto } from './dto/create-car.dto';
-import { UpdateCarDto } from './dto/update-car.dto';
+import { Module } from '@nestjs/common';
+import * as data from './../../data/data.json';
+
+// import { CreateCarDto } from './dto/create-car.dto';
+// import { UpdateCarDto } from './dto/update-car.dto';
 
 @Injectable()
 export class CarsService {
-  create(createCarDto: CreateCarDto) {
-    return 'This action adds a new car';
-  }
+  // create(createCarDto: CreateCarDto) {
+  //   return 'This action adds a new car';
+  // }
 
   findAll() {
     return `This action returns all cars`;
@@ -16,11 +19,11 @@ export class CarsService {
     return `This action returns a #${id} car`;
   }
 
-  update(id: number, updateCarDto: UpdateCarDto) {
-    return `This action updates a #${id} car`;
-  }
+  // update(id: number, updateCarDto: UpdateCarDto) {
+  //   return `This action updates a #${id} car`;
+  // }
 
-  remove(id: number) {
-    return `This action removes a #${id} car`;
-  }
+  // remove(id: number) {
+  //   return `This action removes a #${id} car`;
+  // }
 }
