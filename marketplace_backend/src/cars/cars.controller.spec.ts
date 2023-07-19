ad5f8ce15524450aee946de48cf9142c18416fb6
Leolo Flórez <116220158+LeoloFlrz@users.noreply.github.com>
2023-07-19 18:05:06 +0200

    added tests

--- marketplace_backend/src/cars/cars.controller.spec.ts
+++ marketplace_backend/src/cars/cars.controller.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Test, TestingModule } from '@nestjs/testing';
 import { CarsController } from './cars.controller';
 import { CarsService } from './cars.service';
