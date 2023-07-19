2b68591d5b276405c7c9b93f01e50ddcfcfea3e8
denizozerdogan <131254999+denizozerdogan@users.noreply.github.com>
2023-07-19 19:22:22 +0200

    schema created, services updated

--- marketplace_backend/src/app.module.ts
+++ marketplace_backend/src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CarsModule } from './cars/cars.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [CarsModule],
+  imports: [
+    CarsModule,
+    MongooseModule.forRoot('mongodb://localhost/arabadb', {
+      uri: 'mongodb://localhost/arabadb',
+      connectionName: 'arabadb',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- marketplace_backend/src/cars/cars.service.ts
+++ marketplace_backend/src/cars/cars.service.ts
@@ -1,35 +1,41 @@
 /* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 import * as data from './../../data/data.json';
+import { InjectModel } from '@nestjs/mongoose';
+import { Car } from './schemas/car.schema';
+import { Model } from 'mongoose';
 
 // import { CreateCarDto } from './dto/create-car.dto';
 // import { UpdateCarDto } from './dto/update-car.dto';
 
 @Injectable()
 export class CarsService {
-
-  findAll() {
-    return data;
+  constructor(
+    @InjectModel(Car.name) private carModel: Model<Car>,
+  ) {}
+  
+  async findAll(): Promise<Car[]> {
+    return this.carModel.find().exec();
   }
 
-  findOne(id: string) {
-    // return `This action returns a #${id} beer`;
-    return data.find((e) => e._id == id);
+  async findOne(id: string): Promise<Car | null> {
+    return this.carModel.findById(id).exec();
   }
 
-  searchByProperty(property: string, value: string) {
-    return data.filter((item) => item[property] === value);
+
+  async searchByProperty(property: string, value: string): Promise<Car[]> {
+    const query: Record<string, any> = {};
+    query[property] = value;
+    return this.carModel.find(query).exec();
   }
 
-  searchByKeyword(keyword: string): any[] {
-    const results = data.filter(item => {
-      for (const key in item) {
-        if (typeof item[key] === 'string' && item[key].toLowerCase().includes(keyword.toLowerCase())) {
-          return true;
-        }
-      }
-      return false;
-    });
-    return results;
+  async searchByKeyword(keyword: string): Promise<Car[]> {
+    const regex = new RegExp(keyword, 'i');
+    const query = {
+      $or: Object.keys(this.carModel.schema.paths)
+        .filter((key) => this.carModel.schema.paths[key].instance === 'String')
+        .map((key) => ({ [key]: regex })),
+    };
+    return this.carModel.find(query).exec();
   }
 }
