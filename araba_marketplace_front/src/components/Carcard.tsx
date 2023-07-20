f47f60b75c8845c40b6a3a8ea3b94869789a5302
MiriV <mirivf@hotmail.es>
2023-07-20 15:23:37 +0200

    adding icons to card

--- araba_marketplace_front/src/components/Carcard.tsx
+++ araba_marketplace_front/src/components/Carcard.tsx
@@ -2,23 +2,50 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import './carcard.css';
 import skoda_prueba from '../assets/skoda_prueba.jpg';
+import car from '../assets/car-13dpi.svg';
+import puerta from '../assets/puerta coche3dpi 2.svg';
+import check from '../assets/check3dpi 2.svg';
+import taco from '../assets/tacometro3dpi 1.svg';
+import comb from '../assets/gasolina3dpi 1.svg';
+import map from '../assets/ICONO_MAP.svg';
 
-function Carcard(props) {
+
+
+
+function Carcard() {
   return (
     <Card className='carCard' style={{ width: '18rem' }}>
       <Card.Img className='carImage' variant="top" src={skoda_prueba} />
       <Card.Body>
-        <Card.Title>SKODA SCALA STYLE 1.0 TSI</Card.Title>
+        <Card.Title className='title'>SKODA SCALA STYLE 1.0 TSI</Card.Title>
         <div className='carCharacs'>
           <ul className='cardList'>
-            <li>GERENCIA</li>
-            <li>5 PUERTAS</li>
-            <li>07/2022</li>            
+            <li className='li'>
+              <img src = {car} width="15" height="15"/> 
+              <p>GERENCIA</p> 
+            </li>
+            <li className='li'>
+              <img src = {puerta} width="15" height="15"/>
+              <p>5 PUERTAS</p>
+            </li>
+            <li className='li'>
+              <img src = {map} width="15" height="15"/>
+              <p>07/2022</p>
+            </li>            
           </ul>
           <ul className='cardList'>
-            <li>12821 KM</li>
-            <li>110 CV</li>
-            <li>GASOLINA-V2</li>            
+            <li className='li'>
+              <img src = {check} width="15" height="15"/>
+              <p>12821 KM</p>
+            </li>
+            <li className='li'>
+              <img src = {taco} width="15" height="15"/>
+              <p>110 CV</p>
+            </li>
+            <li className='li'>
+              <img src = {comb} width="15" height="15"/>
+              <p>GASOLINA-V2</p>
+            </li>            
           </ul>
         </div>
         <div className='pricesandbutton'>
