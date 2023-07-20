b486e0eaf4ed8c920c6efd3cbbd78d8c827df372
AleNS <isalejanaranjo@gmail.com>
2023-07-20 14:25:23 +0200

    Getting whatsapp and telegram icons and installing the respective fortawesome dependencies

--- araba_marketplace_front/src/components/Footer.tsx
+++ araba_marketplace_front/src/components/Footer.tsx
@@ -2,9 +2,10 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {  faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import {  faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import ModalFooter from 'react-bootstrap/esm/ModalFooter';
 import Col from 'react-bootstrap/Col';
+import {faWhatsapp,faTelegram} from '@fortawesome/free-brands-svg-icons'
 
 export const Footer = () => {
   return (
@@ -26,8 +27,18 @@ export const Footer = () => {
         <Col xs lg="2">
           <Nav.Item className="me-auto">
                         
-            <FontAwesomeIcon icon={faEnvelope} />
+            <FontAwesomeIcon icon={faEnvelope}className="icono"  />
                               Correo
+          </Nav.Item>
+        </Col>
+        <Col xs lg="2">
+          <Nav.Item className="me-auto">
+            <FontAwesomeIcon icon={faWhatsapp} className="icono" />        
+          </Nav.Item>
+        </Col>
+        <Col xs lg="2">
+          <Nav.Item className="me-auto">
+            <FontAwesomeIcon icon={faTelegram} className="icono" />        
           </Nav.Item>
         </Col>
       </Navbar>
