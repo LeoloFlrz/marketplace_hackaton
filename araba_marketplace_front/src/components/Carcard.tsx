002383383583a400fc93780bb44e69cb666fca4a
Greiskel <greiskel.sanchez@gmail.com>
2023-07-20 15:16:06 +0200

    cambios por ventana modal

--- araba_marketplace_front/src/components/Carcard.tsx
+++ araba_marketplace_front/src/components/Carcard.tsx
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import './css/CarCard.css';
+import skoda_prueba from './assets/skoda_prueba.jpg';
+import React from 'react';
+
 
 interface Car {
   _id: string;
@@ -16,45 +18,37 @@ interface Car {
 }
 
 interface CarCardProps {
-  car: Car;
-}
-
-export const CarCard: React.FC<CarCardProps> = ({ car }) => {
-  const { model, doors, year, km, horsepower, fuelType, price, image_url } = car;
-
-  return (
-    
-
-      <Card className='carCard' style={{ width: '18rem' }}>
-        <Card.Img className='carImage' variant="top" src={image_url} />
-        <Card.Body>
-          <Card.Title><strong>Model:</strong> {model}</Card.Title>
-          <div className='carCharacs'>
-            <ul className='cardList'>
-              <li>{doors} DOORS</li>
-              <li>{year} YEAR</li>
-            </ul>
-            <ul className='cardList'>
-              <li>{km} KM</li>
-              <li>{horsepower} CV</li>
-              <li>{fuelType}</li>
-            </ul>
+    car: Car;
+  }
+  
+  export const CarCard: React.FC<CarCardProps> = ({ car }) => {
+  
+    const { model, doors, year, km, horsepower, fuelType, price, image_url } = car;
+    return (
+        <Card className='carCard' style={{ width: '18rem' }}>
+      <Card.Img className='carImage' variant="top" src={image_url} />
+      <Card.Body>
+        <Card.Title>{model}</Card.Title>
+        <div className='carCharacs'>
+          <ul className='cardList'>
+            <li>{doors} PUERTAS</li>
+            <li>{year}</li>
+          </ul>
+          <ul className='cardList'>
+            <li>{km} KM</li>
+            <li>{horsepower} CV</li>
+            <li>{fuelType}</li>
+          </ul>
+        </div>
+        <div className='pricesandbutton'>
+          <div className='prices'>
+            <p className='fullprice'>{price.toFixed(2)}€</p>
+            {/* Ajusta la forma en que se muestra el pago mensual */}
+            <p className='payments'>desde 305,00€/mes</p>
           </div>
-          <div className='pricesandbutton'>
-            <div className='prices'>
-              <p className='fullprice'>{price.toFixed(2)}€</p>
-              {/* Ajusta la forma en que se muestra el pago mensual */}
-              <p className='payments'>from 305,00€/mes </p>
-            </div>
-       
-           <Button className='button' variant="primary" href='/Cardetail'>
-              More about
-            </Button>
-          </div>
-        </Card.Body>
-      </Card>
-
-
+          <Button className='button' variant="primary">ME INTERESA</Button>
+        </div>
+      </Card.Body>
+    </Card>
   );
 };
-
