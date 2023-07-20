82ba2fe22e5e7f86f7fb629ea056670baad680ac
AleNS <isalejanaranjo@gmail.com>
2023-07-20 09:05:12 +0200

    Reviewed changes and solved conflicts on CarList interface that was missing the image

--- marketplace_backend/src/main.ts
+++ marketplace_backend/src/main.ts
@@ -4,6 +4,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:5173', // Reemplaza con la URL de tu aplicación React
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Si tu aplicación React envía cookies, establece esto en true
+  });
   await app.listen(3000);
 }
 bootstrap();
